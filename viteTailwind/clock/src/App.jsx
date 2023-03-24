652f95b5bafc58a3d8f92e5a50600f851f9a41b9
Yhoan008 <yhoanmoreno8@gmail.com>
2023-03-24 01:03:41 -0500

    Primera version reloj digital

---
+++ viteTailwind/clock/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return(
+    <div className=" w-[100%] h-[100vh]  bg-red-900 flex justify-center items-center" > 
+      <div className="min-w-[400px] min-h-[400px] bg-slate-900 rounded-full " >
+        {console.log("hola mundo")}
+      </div>
+    </div>
+  )
+}
