3376968005e9cd8224ff7b577ba23e50038846d0
Yhoan008 <yhoanmoreno8@gmail.com>
2024-09-23 17:43:41 -0500

    se completa proyecto

--- Javascript/desitionMap/index.js
+++ Javascript/desitionMap/index.js
@@ -3,6 +3,12 @@ const help = document.getElementById("help");
 const back = document.getElementById("back");
 const helpBar = document.getElementById("helpBar");
 const options = document.getElementById("options");
+const clock = document.getElementById("clock");
+const clockBar = document.getElementById("clockBar");
+const menu = document.getElementById("menu");
+const menuBar = document.getElementById("menuBar");
+const reiniciar = document.getElementById("reiniciar");
+const reportError = document.getElementById("reportError");
 
 let base;
 
@@ -21,12 +27,18 @@ getData().then(data => {
 
   function changeIntance(id) {
     instance = instance.product[id];
-    draw(instance, changeIntance)
     history = [...history, instance]
+    draw(instance, history, changeIntance, backInstance)
   }
 
-  draw(data, changeIntance)
+  function backInstance(id) {
+    history = history.slice(0, id + 1)
+    instance = history[history.length - 1]
+    draw(instance, history, changeIntance, backInstance);
+  }
+
   history = [data]
+  draw(data, history, changeIntance, backInstance)
 
 
   back.addEventListener("click", () => {
@@ -35,14 +47,20 @@ getData().then(data => {
     } else {
       history = history.slice(0, -1)
       instance = history[history.length - 1]
-      draw(instance, changeIntance)
+      draw(instance, history, changeIntance, backInstance)
     }
+  })
+
+  reiniciar.addEventListener("click", () => {
+    instance = data;
+    history = [data];
+    draw(instance, history, changeIntance, backInstance)
   })
 
 
 })
 
-function draw(data, changeIntance) {
+function draw(data, history, changeIntance, backInstance) {
   if (data === undefined) { console.error("la instancia es invalida") }
   if (data.help !== undefined) {
     help.style.display = "block";
@@ -52,6 +70,22 @@ function draw(data, changeIntance) {
   }
   question.innerText = `${data.question}`;
 
+  clockBar.innerHTML = "";
+
+  const title = document.createElement('h1');
+  title.textContent = "Historial";
+  title.classList = ('text-xl font-bold  p-2 border-y-2 border-black ');
+  clockBar.appendChild(title); // Añadir el título al contenedor
+
+  history.forEach((index, id) => {
+    const paragraph = document.createElement('p');
+    paragraph.textContent = index.descrip;
+    paragraph.classList = 'text-gray-800 block font-bold h-full py-2 pl-2 hover:bg-gray-300';
+    paragraph.addEventListener("click", () => {
+      backInstance(id);
+    })
+    clockBar.appendChild(paragraph);
+  })
   if (data.type == "multitask") {
 
     const contain = document.createElement("select");
@@ -59,7 +93,7 @@ function draw(data, changeIntance) {
 
     options.innerHTML = "";
 
-    contain.classList = "py-2 px-5  mt-5 rounded font-bold text-xl bg-[#011059] text-[#418cf1]";
+    contain.classList = "py-2 px-5 max-w-[400px] mt-5 rounded font-bold text-xl bg-[#011059] text-white";
     defa.innerText = "Selecciona una opción:"
     contain.appendChild(defa);
     data.options.forEach(element => {
@@ -115,6 +149,34 @@ function draw(data, changeIntance) {
 
   }
 }
+
+let clockActive = false;
+
+clock.addEventListener("click", (event) => {
+  if (!clockBar.contains(event.target)) {
+    clockActive = !clockActive;
+    clockActive ? clockBar.style.display = "block" : clockBar.style.display = "none";
+  }
+})
+
+document.addEventListener("click", (event) => {
+  if (!clock.contains(event.target)) {
+    clockActive = false;
+    clockActive ? clockBar.style.display = "block" : clockBar.style.display = "none";
+  }
+  if (!menu.contains(event.target)) {
+    menuActive = false;
+    menuActive ? menuBar.style.display = "block" : menuBar.style.display = "none"
+
+  }
+})
+
+let menuActive = false;
+
+menu.addEventListener("click", () => {
+  menuActive = !menuActive;
+  menuActive ? menuBar.style.display = "block" : menuBar.style.display = "none"
+})
 
 help.addEventListener("mouseenter", () => {
   helpBar.style.display = "block"
